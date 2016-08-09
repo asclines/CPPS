330bca6b534644d0e4b9936a9a976f2b5a3bd157
Alexander Clines <alexander.clines@ttu.edu>
2016-08-09 17:35:02 -0500

    Added ToperCoder.SRM.695.Java.BearNSWE

---
+++ TopCoder/SRMs/SRM_695/BearNSWE/Java/Solution_01/BearNSWE.java
@@ -0,0 +1,33 @@
+public class BearNSWE {
+
+    public double totalDistance(int[] a, String dir) {
+        int UD = 0;
+        int RL = 0;
+        double result = 0.00;
+        for(int i = 0; i < a.length; i++){
+            result += a[i];
+            char x = dir.charAt(i);
+            switch (x){
+                case 'N':
+                    UD += a[i];
+                    break;
+                case 'S':
+                    UD -= a[i];
+                    break;
+                case 'E':
+                    RL -= a[i];
+                    break;
+                case 'W':
+                    RL += a[i];
+                    break;
+            }
+
+
+        }
+
+        result += Math.sqrt(UD*UD + RL * RL);
+
+
+        return result;
+    }
+}
